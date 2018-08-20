66eb6812aa45ffbf5a5c9012c27b6296628c499a
qoire <mr.sun.yao@gmail.com>
2018-08-20 15:42:26 -0400

    switch to non-secure RNG

--- ethereum-mock-rpc/src/main/java/org/aion/util/MockAddressGenerator.java
+++ ethereum-mock-rpc/src/main/java/org/aion/util/MockAddressGenerator.java
@@ -1,12 +1,13 @@
 package org.aion.util;
 
 import java.security.SecureRandom;
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MockAddressGenerator {
 
-    private static final SecureRandom random = new SecureRandom("MockAddressGenerator".getBytes());
+    private static final Random random = new Random(42L);
     private static final Lock lock = new ReentrantLock();
 
     // TODO: this can be prettier
